a90da6e5759d10d3a881be4582ade1d703957aaf
dawid-kulikowski <dawidkulikowski94@gmail.com>
2022-06-17 21:22:56 +0200

    Coding Excercises 2 - DoubleX

---
+++ src/main/java/DoubleX.java
@@ -0,0 +1,27 @@
+//Given a string, return true if the first instance of "x" in the string is immediately followed by another "x".
+//
+//
+//        doubleX("axxbb") → true
+//        doubleX("axaxax") → false
+//        doubleX("xxxxx") → true
+
+
+public class DoubleX {
+    private String str;
+
+    public DoubleX(String str) {
+        this.str = str;
+    }
+
+    boolean doubleX(String str) {
+        int i = str.indexOf("x");
+        if (i == -1) {
+            return false;
+        }
+
+
+        if (i+1 >= str.length()) return false;
+        return str.substring(i+1, i+2).equals("x");
+
+    }
+}
